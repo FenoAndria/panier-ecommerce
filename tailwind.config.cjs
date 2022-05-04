936fef8224079bc426fc969aedb58c211e307d34
FenoAndria <feno.andriamorasata@gmail.com>
2022-05-04 16:07:31 +0300

    Init :)

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: 'Gill Sans',
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        'green': '#1DB954',
+        'dark': '#121212',
+        'light': '#282828',
+        'lightest': '#B3B3B3',
+        'darkest': '#191414',
+      }
+    },
+  },
+  plugins: [],
+}
