904f4ff7320b6e1386fcf6767c91c33c969ca24b
FenoAndria <feno.andriamorasata@gmail.com>
2022-05-05 09:48:36 +0300

    0.2

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,24 @@
           <div
             v-for="(product, productIndex) in Products"
             :key="productIndex"
-            class="w-1/5 px-1"
+            class="w-1/4 px-1"
           >
             <div class="card bg-violet-500 text-white">
               <h4 class="product-name">{{ product.name }}</h4>
               <hr class="border-gray-200 my-2" />
               <div class="flex justify-between">
                 <span class="badge bg-blue-500">{{ product.price }}€</span>
-                <button class="badge bg-teal-500" @click="addToCart(product)">
-                  <i class="bi bi-cart-plus"></i>
-                </button>
+                <div class="flex space-x-1">
+                  <button class="badge bg-teal-500" @click="dec(product)">
+                    -
+                  </button>
+                  <span class="bg-white text-blue-900 font-bold px-4 rounded">{{
+                    product.quantity ?? 0
+                  }}</span>
+                  <button class="badge bg-teal-500" @click="inc(product)">
+                    +
+                  </button>
+                </div>
               </div>
             </div>
           </div>
@@ -27,7 +35,17 @@
         <h2 class="text-2xl text-white">Panier</h2>
         <hr />
         <div class="card bg-blue-500 text-white text-lg">
-          <div class="flex justify-between text-xl bg-blue-600 rounded px-2 font-semibold">
+          <div
+            class="
+              flex
+              justify-between
+              text-xl
+              bg-blue-600
+              rounded
+              px-2
+              font-semibold
+            "
+          >
             <span class=""
               >{{ this.panier.length }} produit{{
                 this.panier.length > 1 ? "s" : ""
@@ -42,11 +60,14 @@
               class="flex justify-between"
             >
               <div>
-                <span>{{ panierItem.name }}</span>
+                <span class="mr-1">{{ panierItem.name }} </span>
+                <span class="badge bg-orange-500 text-sm"
+                  >{{ panierItem.quantity }}
+                </span>
               </div>
               <div>
                 <span class="font-semibold text-gray-200"
-                  >{{ panierItem.price }} € |
+                  >{{ panierItem.price * panierItem.quantity }} € |
                 </span>
                 <button
                   class="text-red-300 text-sm badge bg-red-400"
@@ -79,7 +100,7 @@ export default {
       let sum = 0;
       if (this.panier.length > 0) {
         sum = this.panier.reduce((total, value) => {
-          return total + value.price;
+          return total + value.price * value.quantity;
         }, 0);
       }
       return sum;
@@ -93,8 +114,25 @@ export default {
       }
     },
     deleteFromCart(product) {
+      product.quantity = 0;
       let productIndex = this.panier.indexOf(product);
       let temp = this.panier.splice(productIndex, 1);
+    },
+    inc(product) {
+      if (!product.quantity) {
+        product.quantity = 0;
+      }
+      product.quantity++;
+      if (!this.panier.includes(product)) {
+        this.panier.push(product);
+      }
+    },
+    dec(product) {
+      if (product.quantity > 1) {
+        product.quantity--;
+      } else {
+          this.deleteFromCart(product);
+      }
     },
   },
   mounted() {
